8704e68b33126c3cf58a87a86a2ffb235c108035
DhivyaBharkavi <52399831+DhivyaBharkavi@users.noreply.github.com>
2019-08-12 09:47:48 +0530

    Exercise 12 august 2019
    Array using right shift once and shift twice

--- RightShift.java
+++ RightShift.java
@@ -22,27 +22,49 @@ public class RightShift {
 	}
 	public void shiftingonce()
 	{
-		System.out.println("Shifting once");
-		int temp=a[0];
-		while(i<a.length)
+		System.out.println("First element to last position also shifting once");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		int temp=a[0];i=0;
+		while(i<a.length-1)
 		{
-			System.out.println("I is "+i+" a["+i+"]");
+			//System.out.println("I is "+i+" a["+i+"]");
 			a[i+1]=a[i];
 			i++;
 			
 		}
-		a[i-1]=temp;
+		a[a.length-1]=temp;
 		for(i=0;i<5;i++)
 		{
 		System.out.print(a[i]+"\t");
 		}
 		System.out.println();
 	}
-	
+		public void shiftingtwice()
+	{
+		System.out.println("First and second element to last and previous position also shifting twice");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		int temp=a[0],i=0;
+		int temp1=a[1];
+		while(i<a.length-2)
+		{
+			//System.out.println("I is "+i+" a["+i+"]");
+			a[i]=a[i+1];
+			i++;
+			
+		} 
+		a[a.length-1]=temp;
+		a[a.length-2]=temp1;
+		for(i=0;i<5;i++)
+		{
+		System.out.print(a[i]+"\t");
+		}
+ 		System.out.println();
+	}	
 	public static void main(String[] args) {
 		RightShift rs = new RightShift();
 		rs.getInput();
 		rs.shiftingonce();
+		a.shiftingtwice();
 	}
 
 }
